ff41ad261443279e3dc2c64e1727c79171d12212
14loop <lidiarzc@gmail.com>
2024-12-07 08:27:31 -0700

    Update login.php
     testing to see if adding /wwwroot/ will fix redirect issues in azure

--- login.php
+++ login.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-require_once(__DIR__ . '/config.php'); // Ensure correct path
+require_once(__DIR__ . '/wwwroot/config.php'); // Ensure correct path
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $email = $_POST['email'];
