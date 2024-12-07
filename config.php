2b53cdfb88c3917256e76298684b6aa538d4320a
14loop <lidiarzc@gmail.com>
2024-12-06 19:54:43 -0700

    Update config.php
    troubleshooting sql connection to database

--- config.php
+++ config.php
@@ -5,8 +5,12 @@ $username = "zstafqrshb";
 $password = "CKlyc1XfXVR3Rz1$"; 
 $database = "pawsandpurrs-database"; // Your database name
 
-// Create a connection
-$conn = new mysqli($host, $username, $password, $database);
+//Establishes the connection
+$conn = mysqli_init();
+mysqli_real_connect($conn, $host, $username, $password, $db_name, 3306);
+if (mysqli_connect_errno($conn)) {f
+die('Failed to connect to MySQL: '.mysqli_connect_error());
+}
 
 // Check connection
 if ($conn->connect_error) {
